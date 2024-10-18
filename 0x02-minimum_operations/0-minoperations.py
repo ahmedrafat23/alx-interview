c2e73d72e34f9d94ea57933f107656484d49dfcd
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2024-10-18 00:10:59 +0000

    first edition

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+
+
 def minOperations(n):
     if n <= 1:
         return 0
+    
     operations = 0
-    divisor = 2
+    factor = 2
+    
     while n > 1:
-        if n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        else:
-            divisor += 1
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
     return operations
-
