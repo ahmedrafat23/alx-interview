54bbb2c4df2a89ee0851f9cccec180b7fd227c74
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2024-10-18 23:39:45 +0000

    a

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
+minOperations = __import__('0-minoperations').minOperations
 
+print(minOperations(0))    # 0
+print(minOperations(1))    # 0
+print(minOperations(4))    # 4
+print(minOperations(12))   # 7
+print(minOperations(19170307)) # Expected value according to problem logic
 
-def minOperations(n):
-    if n <= 1:
-        return 0
-    
-    operations = 0
-    factor = 2
-    
-    while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
-    
-    return operations
