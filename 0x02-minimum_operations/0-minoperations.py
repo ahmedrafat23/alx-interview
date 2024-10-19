172649d8c795fe3920b0ba7733fe311360f8e759
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2024-10-19 15:17:58 +0000

    edition

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-
+"""
+This module calculates the minimum number of operations required
+to result in exactly n 'H' characters using only Copy All and Paste.
+"""
 
 def minOperations(n):
+    """
+    Returns the minimum number of operations to get exactly n 'H' characters.
+    
+    If n is impossible to achieve or is less than 1, return 0.
+    """
     if n <= 1:
-        return 0
+        return 0  # No operations needed if n <= 1
     
-    operations = 0
-    factor = 2
+    operations = 0  # This will hold the total number of operations
+    divisor = 2  # Start checking with the smallest divisor, which is 2
     
+    # Continue until we reduce n to 1 by dividing it by its divisors
     while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
+        # If n is divisible by the current divisor
+        while n % divisor == 0:
+            operations += divisor  # Add the divisor to the count of operations
+            n //= divisor  # Reduce n by dividing it by the divisor
+        divisor += 1  # Move to the next divisor
     
-    return operations
+    return operations  # Return the total number of operations needed
+
