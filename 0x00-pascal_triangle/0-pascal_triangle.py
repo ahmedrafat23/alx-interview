56b332d320a280671572c7f9b5e097d1f41e33c7
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2024-10-20 17:36:30 +0000

    alx-interview

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,30 @@
+#!/usr/bin/python3
+"""
+This module contains a function to generate Pascal's Triangle.
+"""
+
 def pascal_triangle(n):
+    """
+    Returns a list of lists representing Pascal’s triangle of size n.
+
+    Args:
+        n (int): Number of rows in the triangle.
+
+    Returns:
+        List of lists of integers.
+    """
     if n <= 0:
         return []
+    
+    # The first row
+    triangle = [[1]]
 
-    triangle = [[1]]  # Start with the first row
-
+    # Construct the remaining rows
     for i in range(1, n):
         prev_row = triangle[-1]
-        # Start each row with a 1
         new_row = [1]
-        # Calculate the in-between values using previous row
         for j in range(1, i):
             new_row.append(prev_row[j - 1] + prev_row[j])
-        # End each row with a 1
         new_row.append(1)
         triangle.append(new_row)
 
